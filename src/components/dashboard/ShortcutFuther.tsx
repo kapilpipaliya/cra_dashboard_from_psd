19344b68f08eedc1c4cee7754287d6627be6617c
kapil <kapil.pipaliya@yahoo.com>
2021-06-01 11:53:25 +0530

    add shortcut-feature component

--- src/components/dashboard/ShortcutFuther.tsx
+++ src/components/dashboard/ShortcutFuther.tsx
@@ -1,4 +1,59 @@
 import * as React from "react";
+import clsx from "clsx";
 
-interface ShortcutFutherProps {}
-const ShortcutFuther = (props: ShortcutFutherProps) => {};
+interface CardProps {
+  title: string;
+  subtitle: string;
+  iconClassName: string;
+  variant: "normal" | "warning" | "success" | "danger";
+}
+const Card = (props: CardProps) => {
+  const { title, subtitle, iconClassName, variant } = props;
+  return (
+    <div className="flex-vertical card">
+      <div className="flex-horizontal flex-align-items-center">
+        <div className="flex-vertical">
+          <div className={clsx("title", variant)}>{title}</div>
+          <div className="subtitle">{subtitle}</div>
+        </div>
+        <div className="count">
+          <i className={iconClassName} />
+        </div>
+      </div>
+      <div className={clsx("flex-horizontal footer", variant)}>
+        <div className="change">% change</div>
+        <i className="fas fa-chart-bar" />
+      </div>
+    </div>
+  );
+};
+export const ShortcutFuther = () => {
+  return (
+    <div className="flex-horizontal shortcut-feature">
+      <Card
+        title="66,163K"
+        subtitle="Total Leads"
+        iconClassName="fas fa-shopping-bag"
+        variant="normal"
+      />
+      <Card
+        title="3,490K"
+        subtitle="Total Pending"
+        iconClassName="far fa-times-circle"
+        variant="danger"
+      />
+      <Card
+        title="38,372K"
+        subtitle="Case Closed"
+        iconClassName="fas fa-lock"
+        variant="success"
+      />
+      <Card
+        title="5,355K"
+        subtitle="Task Completed"
+        iconClassName="fas fa-paste"
+        variant="warning"
+      />
+    </div>
+  );
+};
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { Menu } from "./dashboard/Menu";
 import { Navbar } from "./dashboard/Navbar";
 import { WelcomeToInfinity } from "./dashboard/WelcomeToInfinity";
+import { ShortcutFuther } from "./dashboard/ShortcutFuther";
 interface DashboardProps {}
 
 export const Dashboard = (props: DashboardProps) => {
@@ -12,6 +13,7 @@ export const Dashboard = (props: DashboardProps) => {
         <Navbar />
         <div className="flex-grow-1 dashboard-content">
           <WelcomeToInfinity />
+          <ShortcutFuther />
         </div>
       </div>
     </>
