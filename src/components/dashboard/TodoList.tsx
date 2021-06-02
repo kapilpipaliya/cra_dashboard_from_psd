47f7caed90196854f8d5dd34b6ad8f43d32bf4c1
kapil <kapil.pipaliya@yahoo.com>
2021-06-02 20:08:47 +0530

    make separate component for TodoItem

--- src/components/dashboard/TodoList.tsx
+++ src/components/dashboard/TodoList.tsx
@@ -1,4 +1,24 @@
 import * as React from "react";
+import clsx from "clsx";
+
+interface TodoItemProps {
+  id: string;
+  todo: string;
+  className?: string;
+  defaultChecked?: boolean;
+}
+
+const TodoItem = (props: TodoItemProps) => {
+  const { id, todo, className, defaultChecked } = props;
+  return (
+    <li className="todo-item">
+      <div className={clsx("checkbox", className)}>
+        <input type="checkbox" id={id} defaultChecked={defaultChecked} />
+        <label htmlFor={id}>{todo}</label>
+      </div>
+    </li>
+  );
+};
 
 export const TodoList = () => (
   <div className="widget todo-widget">
@@ -8,49 +28,40 @@ export const TodoList = () => (
     <hr className="widget-separator" />
     <div className="widget-body p-b-0">
       <ul className="todo-list">
-        <li className="todo-item">
-          <div className="checkbox checkbox-default">
-            <input type="checkbox" id="checkbox02" />
-            <label htmlFor="checkbox02">
-              Record The First Episode Of HTML Tutorial
-            </label>
-          </div>
-        </li>
-
-        <li className="todo-item">
-          <div className="checkbox checkbox-pink">
-            <input type="checkbox" id="checkbox2" />
-            <label htmlFor="checkbox2">Prepare The Conference Schedule</label>
-          </div>
-        </li>
-
-        <li className="todo-item">
-          <div className="checkbox checkbox-warning">
-            <input type="checkbox" id="checkbox4" defaultChecked />
-            <label htmlFor="checkbox4">Decide The Live Discussion Time</label>
-          </div>
-        </li>
-
-        <li className="todo-item">
-          <div className="checkbox checkbox-success">
-            <input type="checkbox" id="checkbox3" defaultChecked />
-            <label htmlFor="checkbox3">Prepare For The Next Project</label>
-          </div>
-        </li>
-
-        <li className="todo-item">
-          <div className="checkbox checkbox-danger">
-            <input type="checkbox" id="checkbox5" defaultChecked />
-            <label htmlFor="checkbox5">Finish Up AngularJs Tutorial</label>
-          </div>
-        </li>
-
-        <li className="todo-item">
-          <div className="checkbox checkbox-purple">
-            <input type="checkbox" id="checkbox1" defaultChecked />
-            <label htmlFor="checkbox1">Finish Infinity Project</label>
-          </div>
-        </li>
+        <TodoItem
+          id="checkbox1"
+          todo="Record The First Episode Of HTML Tutorial"
+          className="checkbox-default"
+        />
+        <TodoItem
+          id="checkbox2"
+          todo="Prepare The Conference Schedule"
+          className="checkbox-pink"
+        />
+        <TodoItem
+          id="checkbox3"
+          todo="Decide The Live Discussion Time"
+          className="checkbox-warning"
+          defaultChecked
+        />
+        <TodoItem
+          id="checkbox4"
+          todo="Prepare For The Next Project"
+          className="checkbox-success"
+          defaultChecked
+        />
+        <TodoItem
+          id="checkbox5"
+          todo="Finish Up AngularJs Tutorial"
+          className="checkbox-danger"
+          defaultChecked
+        />
+        <TodoItem
+          id="checkbox6"
+          todo="Finish Infinity Project"
+          className="checkbox-purple"
+          defaultChecked
+        />
       </ul>
     </div>
     <div className="new-todo">
