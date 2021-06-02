f135712270f457b03796944f383a4bee62c29db6
kapil <kapil.pipaliya@yahoo.com>
2021-06-02 20:29:30 +0530

    7.make separate constant for todo data and map it.

--- src/components/dashboard/TodoList.tsx
+++ src/components/dashboard/TodoList.tsx
@@ -1,6 +1,57 @@
 import * as React from "react";
 import clsx from "clsx";
 
+interface TodoItem {
+  id: string;
+  todo: string;
+  className: string;
+  completed: boolean;
+}
+
+const todoData: TodoItem[] = [
+  {
+    id: "checkbox1",
+    todo: "Record The First Episode Of HTML Tutorial",
+    className: "checkbox-default",
+    completed: false,
+  },
+
+  {
+    id: "checkbox2",
+    todo: "Prepare The Conference Schedule",
+    className: "checkbox-pink",
+    completed: false,
+  },
+
+  {
+    id: "checkbox3",
+    todo: "Decide The Live Discussion Time",
+    className: "checkbox-warning",
+    completed: true,
+  },
+
+  {
+    id: "checkbox4",
+    todo: "Prepare For The Next Project",
+    className: "checkbox-success",
+    completed: true,
+  },
+
+  {
+    id: "checkbox5",
+    todo: "Finish Up AngularJs Tutorial",
+    className: "checkbox-danger",
+    completed: true,
+  },
+
+  {
+    id: "checkbox6",
+    todo: "Finish Infinity Project",
+    className: "checkbox-purple",
+    completed: true,
+  },
+];
+
 interface TodoItemProps {
   id: string;
   todo: string;
@@ -28,40 +79,14 @@ export const TodoList = () => (
     <hr className="widget-separator" />
     <div className="widget-body p-b-0">
       <ul className="todo-list">
-        <TodoItem
-          id="checkbox1"
-          todo="Record The First Episode Of HTML Tutorial"
-          className="checkbox-default"
-        />
-        <TodoItem
-          id="checkbox2"
-          todo="Prepare The Conference Schedule"
-          className="checkbox-pink"
-        />
-        <TodoItem
-          id="checkbox3"
-          todo="Decide The Live Discussion Time"
-          className="checkbox-warning"
-          defaultChecked
-        />
-        <TodoItem
-          id="checkbox4"
-          todo="Prepare For The Next Project"
-          className="checkbox-success"
-          defaultChecked
-        />
-        <TodoItem
-          id="checkbox5"
-          todo="Finish Up AngularJs Tutorial"
-          className="checkbox-danger"
-          defaultChecked
-        />
-        <TodoItem
-          id="checkbox6"
-          todo="Finish Infinity Project"
-          className="checkbox-purple"
-          defaultChecked
-        />
+        {todoData.map((todo) => (
+          <TodoItem
+            id={todo.id}
+            todo={todo.todo}
+            className={todo.className}
+            defaultChecked={todo.completed}
+          />
+        ))}
       </ul>
     </div>
     <div className="new-todo">
